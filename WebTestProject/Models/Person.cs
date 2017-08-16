d279fbf35b9b328793d58fe4dcea630752dff78c
jeferson <jeferson@OMEGAB51>
2017-08-16 20:04:43 -0300

    Adicionar arquivos de projeto.

---
+++ WebTestProject/Models/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebTestProject.Models
+{
+    public class Person
+    {
+        
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Old { get; set; }
+        public int Id { get; internal set; }
+    }
+}

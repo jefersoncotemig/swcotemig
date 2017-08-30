c16703aa255dfad89be8074fffaff2abf2096ba1
jeferson <jeferson@cotemig.com.br>
2017-08-30 20:30:46 -0300

    add client

---
+++ ClientWebTestProject/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientWebTestProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var client = new HttpClient())
+            {
+                var id = 10;
+
+                Console.ReadLine();
+                var expectresult = client.GetStringAsync($"http://localhost:8080/v1/persons");
+                var result = expectresult.Result;
+                Console.WriteLine(result);
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
